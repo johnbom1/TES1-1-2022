215883cb3007589089f77a20bc47f23bebbe8787
Jonatan <jonatanfragoso@gmail.com>
2022-08-15 16:51:21 -0500

    arquivo identado

--- atividade_tesi.py
+++ atividade_tesi.py
@@ -13,7 +13,8 @@ class Tela:
         self.frm_botoes.pack(side=tk.BOTTOM)
 
         colunas = ['nome', 'cpf', 'email']
-        self.tvw = ttk.Treeview(self.janela, show='headings', columns=colunas, height=5)
+        self.tvw = ttk.Treeview(
+            self.janela, show='headings', columns=colunas, height=5)
         self.tvw.pack(side=tk.LEFT, fill=tk.BOTH)
 
         # cabeçalho
@@ -25,33 +26,43 @@ class Tela:
         self.tvw.column('cpf', minwidth=0, width=150)
         self.tvw.column('email', minwidth=0, width=180)
 
-        self.tvw.insert('', 'end', values=('Manoel', '0000000000', 'manoel@ufac.br'))
-        self.tvw.insert('', 'end', values=('Manoel', '0000000000', 'manoel@ufac.br'))
-        self.tvw.insert('', 'end', values=('Manoel', '0000000000', 'manoel@ufac.br'))
-        self.tvw.insert('', 'end', values=('Limeira', '1111111111', 'limeira@ufac.br'))
-        self.tvw.insert('', 'end', values=('Limeira', '1111111111', 'limeira@ufac.br'))
-        self.tvw.insert('', 'end', values=('Limeira', '1111111111', 'limeira@ufac.br'))
+        self.tvw.insert('', 'end', values=(
+            'Manoel', '0000000000', 'manoel@ufac.br'))
+        self.tvw.insert('', 'end', values=(
+            'Manoel', '0000000000', 'manoel@ufac.br'))
+        self.tvw.insert('', 'end', values=(
+            'Manoel', '0000000000', 'manoel@ufac.br'))
+        self.tvw.insert('', 'end', values=(
+            'Limeira', '1111111111', 'limeira@ufac.br'))
+        self.tvw.insert('', 'end', values=(
+            'Limeira', '1111111111', 'limeira@ufac.br'))
+        self.tvw.insert('', 'end', values=(
+            'Limeira', '1111111111', 'limeira@ufac.br'))
 
         self.scr = ttk.Scrollbar(self.janela, command=self.tvw.yview)
         self.scr.pack(side=tk.LEFT, fill=tk.BOTH)
         self.tvw.configure(yscroll=self.scr.set)
 
         # botoes
-        self.btn_cadastrar = tk.Button(self.frm_botoes, text='Cadastrar', command=self.tela_cadastrar)
+        self.btn_cadastrar = tk.Button(
+            self.frm_botoes, text='Cadastrar', command=self.tela_cadastrar)
         self.btn_cadastrar.pack(side=tk.LEFT)
         #self.btn_deletar = tk.Button(self.frm_botoes, text='Deletar', command=self.deletar_selecionado)
-        #self.btn_deletar.pack(side=tk.LEFT)
+        # self.btn_deletar.pack(side=tk.LEFT)
 
-        self.btn_deletar_todos = tk.Button(self.frm_botoes, text='Deletar Todos', command=self.deletar_todos)
+        self.btn_deletar_todos = tk.Button(
+            self.frm_botoes, text='Deletar Todos', command=self.deletar_todos)
         self.btn_deletar_todos.pack(side=tk.LEFT)
-        self.btn_deletar_lista = tk.Button(self.frm_botoes, text='Deletar Selecionados', command=self.deletar_lista)
+        self.btn_deletar_lista = tk.Button(
+            self.frm_botoes, text='Deletar Selecionados', command=self.deletar_lista)
         self.btn_deletar_lista.pack(side=tk.LEFT)
 
-        self.btn_atualizar = tk.Button(self.frm_botoes, text='Atualizar', command=self.tela_atualizar)
+        self.btn_atualizar = tk.Button(
+            self.frm_botoes, text='Atualizar', command=self.tela_atualizar)
         self.btn_atualizar.pack(side=tk.LEFT)
 
     def tela_atualizar(self):
-        #pegando informações do treeview
+        # pegando informações do treeview
         selecionado = self.tvw.selection()
         contador = 0
         contador = contador + len(self.tvw.selection())
@@ -69,20 +80,7 @@ class Tela:
             self.lbl_cpf.grid(row=1, column=0)
             self.lbl_email = tk.Label(self.top_atualizar, text='Email:')
             self.lbl_email.grid(row=2, column=0)
-            lista = self.tvw.item(selecionado, 'values')
-            self.ent_nome = tk.Entry(self.top_atualizar, width=30)
-            self.ent_nome.grid(row=0, column=1)
-            self.ent_nome.insert(0, lista[0])
 
-            self.ent_cpf = tk.Entry(self.top_atualizar, width=30)
-            self.ent_cpf.grid(row=1, column=1)
-            self.ent_cpf.insert(0, lista[1])
-
-            self.ent_email = tk.Entry(self.top_atualizar, width=30)
-            self.ent_email.grid(row=2, column=1)
-            self.ent_email.insert(0, lista[2])
-        
-            
             lista = self.tvw.item(selecionado, 'values')
             self.ent_nome = tk.Entry(self.top_atualizar, width=30)
             self.ent_nome.grid(row=0, column=1)
@@ -96,13 +94,25 @@ class Tela:
             self.ent_email.grid(row=2, column=1)
             self.ent_email.insert(0, lista[2])
 
-            self.btn_confirmar = tk.Button(self.top_atualizar, text='Confirmar Atualização', command=self.confirmar_atualiza)
+            lista = self.tvw.item(selecionado, 'values')
+            self.ent_nome = tk.Entry(self.top_atualizar, width=30)
+            self.ent_nome.grid(row=0, column=1)
+            self.ent_nome.insert(0, lista[0])
+
+            self.ent_cpf = tk.Entry(self.top_atualizar, width=30)
+            self.ent_cpf.grid(row=1, column=1)
+            self.ent_cpf.insert(0, lista[1])
+
+            self.ent_email = tk.Entry(self.top_atualizar, width=30)
+            self.ent_email.grid(row=2, column=1)
+            self.ent_email.insert(0, lista[2])
+
+            self.btn_confirmar = tk.Button(
+                self.top_atualizar, text='Confirmar Atualização', command=self.confirmar_atualiza)
             self.btn_confirmar.grid(row=3, column=1)
 
         else:
-            messagebox.showerror("Erro!", "Nenhum usuário selecionado!")
-            
-
+            messagebox.showwarning("Erro!", "Nenhum usuário selecionado!")
 
     def confirmar_atualiza(self):
         nome = self.ent_nome.get()
@@ -111,13 +121,10 @@ class Tela:
         selecionado = self.tvw.selection()
         self.tvw.item(selecionado, values=(nome, cpf, email))
         self.top_atualizar.destroy()
-        self.janela.deiconify() 
-
-
-            
+        self.janela.deiconify()
 
     def deletar_todos(self):
-        var = messagebox.askquestion("Cuidado","Você está certo disso?")
+        var = messagebox.askquestion("Cuidado", "Você está certo disso?")
         if var == "yes":
             todos = self.tvw.get_children()
             for t in todos:
@@ -128,14 +135,13 @@ class Tela:
         for l in lista:
             self.tvw.delete(l)
 
-
     def deletar_selecionado(self):
         selecionado = self.tvw.selection()
         self.tvw.delete(selecionado)
 
     def tela_cadastrar(self):
         self.top_cadastro = tk.Toplevel()
-        #self.janela.withdraw()
+        # self.janela.withdraw()
         self.top_cadastro.grab_set()
         self.top_cadastro.title('Cadastro')
         self.top_cadastro.geometry('300x200')
@@ -151,7 +157,8 @@ class Tela:
         self.ent_cpf.grid(row=1, column=1)
         self.ent_email = tk.Entry(self.top_cadastro, width=30)
         self.ent_email.grid(row=2, column=1)
-        self.btn_confirmar = tk.Button(self.top_cadastro, text='Confirmar', command=self.confirmar_cadastro)
+        self.btn_confirmar = tk.Button(
+            self.top_cadastro, text='Confirmar', command=self.confirmar_cadastro)
         self.btn_confirmar.grid(row=3, column=1)
 
     def confirmar_cadastro(self):
@@ -159,7 +166,8 @@ class Tela:
         cpf = self.ent_cpf.get()
         email = self.ent_email.get()
         if nome == '' or cpf == '' or email == '':
-            messagebox.showinfo('Aviso', 'Todos campos são obrigatórios', parent=self.top_cadastro)
+            messagebox.showinfo(
+                'Aviso', 'Todos campos são obrigatórios', parent=self.top_cadastro)
         else:
             self.tvw.insert('', 'end', values=(nome, cpf, email))
             self.top_cadastro.destroy()
